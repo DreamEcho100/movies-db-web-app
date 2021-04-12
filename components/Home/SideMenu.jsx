1abe12370dda5d1f9db4234ea64f09c3cea236ec
DreamEcho100 <maze6572198@gmail.com>
2021-04-12 17:08:13 +0200

    Adds modal + close Modal

--- components/Home/SideMenu.jsx
+++ components/Home/SideMenu.jsx
@@ -1,19 +1,15 @@
+import { useState } from 'react';
+
+import Modal from './Modal';
+
 const SideMenu = ({ catagories }) => {
 	return (
 		<>
+			<Modal />
 			<h1 style={{ fontSize: 'calc(var(--header1) - 1rem)' }} className='my-4'>
 				Shop Name
 			</h1>
 			<div className='current-theme list-group'>
-				{/* <a href='#' className='current-theme list-group-item'>
-					Category 1
-				</a>
-				<a href='#' className='current-theme list-group-item'>
-					Category 2
-				</a>
-				<a href='#' className='current-theme list-group-item'>
-					Category 3
-				</a> */}
 				{catagories.map(({ id, name }) => (
 					<a key={id} href='#' className='current-theme list-group-item'>
 						{name}
